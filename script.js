ea5fcd4544d90f36911b0bc02a4ae1ee4f7e30f6
Lyubomir Cholakov <l.cholakoff@gmail.com>
2021-11-09 20:35:40 +0100

    Created UI. Implemented game logic in UI.
    TODO: Fix bug multiple instances of timeout for scoreText fade;
    TODO: Add overall round counter and declare overall winner;
    TODO: Animations for 'slot machine' style computer icons, enlarge selected option for player.

--- script.js
+++ script.js
@@ -1,147 +1,129 @@
 // Rock = 1, Paper = 2, Scissors = 3
 
 // Declare consts for messages
-const youWonMessage = "\n=== Y O U   W O N ! ===\n Congratulations, go grab a beer!"
-const youLostMessage = "\n=== Y O U   L O S T ! ===\n Poor you, go cry in the corner..."
+const youWonMessage = "\n=== Y O U   W O N ! ===\n Congratulations, go grab a beer!";
+const youLostMessage = "\n=== Y O U   L O S T ! ===\n Poor you, go cry in the corner...";
 const youAreTiedMessage = "\n=== I T ' S   A   T I E ! ===\n Not bad, not bad..."
 
-let playerResult = 0
-let computerResult = 0
+let playerScore = 0;
+let computerScore = 0;
 
-game()
-console.log("Refresh the page to try again.")
+const buttons = document.querySelectorAll('.playerBtn');
+buttons.forEach(playerBtn =>
+    playerBtn.addEventListener('click',
+        function () { playRound(playerBtn.getAttribute('data-index'), computerPlay()) }));
+
+// console.log("Refresh the page to try again.");
 
 // FUNCTIONS //
 
 function game() {
-    for (let i = 1; i <= 5; ++i) {
-        const playerSelection = normalizeInput(prompt("What do you choose? Rock, Paper or Scissors?"))
-        const computerSelection = computerPlay()
+    const playerSelection = normalizeInput(prompt("What do you choose? Rock, Paper or Scissors?"));
+    const computerSelection = computerPlay();
 
-        console.log(`R O U N D   ${i}`)
-        console.log(playRound(playerSelection, computerSelection))
-        console.log("--------------------------")
-    }
+    console.log(`R O U N D   ${i}`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("--------------------------");
 
-    console.log("--------------------------")
-    console.log(score())
+    console.log("--------------------------");
+    console.log(score());
 }
 
-// Display overall winner
-function score() {
-    let scoreTable = (`Y O U\t\tCOMPUTER\n  ${playerResult}\t\t   ${computerResult}\n`)
-    if (playerResult === computerResult) {
-        return scoreTable + youAreTiedMessage
-    }
-    else if (playerResult > computerResult) {
-        return scoreTable + youWonMessage
-    }
-    else {
-        return scoreTable + youLostMessage
-    }
-}
-
-
-// Computer Play. Returns a random number 1 - 3
-function computerPlay() {
-    return Math.floor(Math.random() * 3 + 1) // Random number between 1 and 3
-}
-
-// Verify input. 
-// If valid input -> compare with computer and display result.
-// If bad input message.
+// Play one round
 function playRound(playerSelection, computerSelection) {
-    // Input safety check
+    console.log(`Computer chose ${computerSelection}`);
+    console.log(`You chose ${playerSelection}`);
+    const scoreText = document.querySelector('#roundScore');
 
-    if (isValidSelection(playerSelection)) {
-        playerSelection = wordToInt(playerSelection)
-        console.log(`Computer chose ${intToWord(computerSelection)}`)
-        console.log(`You chose ${intToWord(playerSelection)}`)
-
-        if (isTie(playerSelection, computerSelection)) {
-            playerResult++
-            computerResult++
-            return (`You are tied! ${intToWord(playerSelection)} matches ${intToWord(computerSelection)}.`)
-        }
-        else if (playerWins(playerSelection, computerSelection)) {
-            playerResult += 2
-            return `You win! ${intToWord(playerSelection)} beats ${intToWord(computerSelection)}.`
-        }
-        else {
-            computerResult += 2
-            return `You lose! ${intToWord(computerSelection)} beats ${intToWord(playerSelection)}.`
-        }
+    if (isTie(playerSelection, computerSelection)) {
+        playerScore++;
+        computerScore++;
+        let verb = playerSelection === 'scissors' ? 'match' : 'matches'; // This is an ugly hack for plural
+        scoreText.innerText = `${capitalize(playerSelection)} ${verb} ${computerSelection}.\nYou are tied!`;
+    }
+    else if (playerWins(playerSelection, computerSelection)) {
+        playerScore += 2;
+        let verb = playerSelection === 'scissors' ? 'beat' : 'beats'; // This is an ugly hack for plural
+        scoreText.innerText = `${capitalize(playerSelection)} ${verb} ${computerSelection}.\nYou win!`;
     }
     else {
-        return ("I didn't understand that =/")
+        computerScore += 2;
+        let verb = computerSelection === 'scissors' ? 'beat' : 'beats'; // This is an ugly hack for plural
+        scoreText.innerText = `${capitalize(computerSelection)} ${verb} ${playerSelection}.\nYou lose!`;
     }
+    document.querySelector('.computerScore').innerText = computerScore + 'p';
+    document.querySelector('.playerScore').innerText = playerScore + 'p';
+
+    // Reset before fade effect
+    scoreText.style.display = 'block';
+    scoreText.style.opacity = 1;
+    setTimeout(function(){ fade(scoreText); }, 1500);
 }
 
-// Convert word to integer
-function wordToInt(string) {
-    switch (string) {
-        case "rock":
-            return 1
-            break
-        case "paper":
-            return 2
-            break
-        case "scissors":
-            return 3
-            break
-        default:
-            return null // something went wrong
-    }
+// Capitalize first letter
+function capitalize (str) {
+    return str.slice(0,1).toUpperCase() + str.slice (1);
 }
 
-// Convert integer to word
-function intToWord(number) {
-    switch (number) {
-        case 1:
-            return "Rock"
-            break
+// Computer Play. Returns a random rps word
+function computerPlay() {
+    switch (Math.floor(Math.random() * 3 + 1)) {
+        case 1: 
+            return 'rock';
+            break;
         case 2:
-            return "Paper"
-            break
+            return 'paper';
+            break;
         case 3:
-            return "Scissors"
-            break
-        default:
-            return null // something went wrong
-    }
-}
-
-// Normalize player input (convert to lowercase and remove spaces)
-function normalizeInput(string) {
-    if (string !== null)
-        return string.replace(/\s+/g, '').toLowerCase()
-}
-
-// Verify user input and return bool
-function isValidSelection(string) {
-    switch (string) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            return true
-            break
-        default:
-            return false
-    }
+            return 'scissors';
+            break;
+        default: return null;
+    }; // Random number between 1 and 3 converted to rps word
 }
 
 // Return true if it's a tied score
 function isTie(playerSelection, computerSelection) {
-    return playerSelection === computerSelection
+    return playerSelection === computerSelection;
 }
 
 // Return true if player wins
 function playerWins(playerSelection, computerSelection) {
-    if (playerSelection === 1 && computerSelection === 3) {
-        return true
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return true;
     }
-    else if (playerSelection === 3 && computerSelection === 1) {
-        return false
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return true;
     }
-    else return (playerSelection > computerSelection)
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return true;
+    }
+    else return false;
+}
+
+// Display overall winner
+function score() {
+    let scoreTable = (`Y O U\t\tCOMPUTER\n  ${playerScore}\t\t   ${computerScore}\n`);
+    if (playerScore === computerScore) {
+        return scoreTable + youAreTiedMessage;
+    }
+    else if (playerScore > computerScore) {
+        return scoreTable + youWonMessage;
+    }
+    else {
+        return scoreTable + youLostMessage;
+    }
+}
+
+// Fade out
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.3){
+            clearInterval(timer);
+            // element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.01;
+    }, 20);
 }
